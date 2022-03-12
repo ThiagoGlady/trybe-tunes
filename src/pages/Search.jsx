b36ea68ce9b4f20ecc8e15d3fd16d39c9e4ee5a3
ThiagoGlady <thiagoglady@gmail.com>
2022-03-12 17:04:33 -0300

    Requisito 5:ok

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,57 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.onNameChange = this.onNameChange.bind(this);
+
+    this.state = {
+      artistName: '',
+      isButtonDisabled: true,
+    };
+  }
+
+  onNameChange({ target }) {
+    const { value } = target;
+    this.setState({
+      artistName: value,
+    }, () => {
+      if (value.length >= 2) {
+        this.setState({
+          isButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isButtonDisabled: true,
+        });
+      }
+    });
+  }
+
   render() {
+    const { artistName, isButtonDisabled } = this.state;
+    const { onNameChange } = this;
     return (
       <div data-testid="page-search">
         <Header />
-        Gosto de amor
+        <form>
+          <label htmlFor="artistName">
+            <input
+              type="text"
+              placeholder="Nome do Artista"
+              data-testid="search-artist-input"
+              value={ artistName }
+              onChange={ onNameChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isButtonDisabled }
+            >
+              Pesquisar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
